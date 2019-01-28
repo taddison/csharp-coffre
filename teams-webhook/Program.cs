1e92471fcc96d041693e0b37bf7ecd47360a455b
Tim Addison <git@tjaddison.com>
2019-01-28 18:18:53 -0500

    app loads config

---
+++ teams-webhook/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.FileExtensions;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace teams_webhook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile("appsettings.local.json", true, true)
+                .Build();
+
+            Console.WriteLine($"Hello {config["TargetUri"]}");
+        }
+    }
+}
