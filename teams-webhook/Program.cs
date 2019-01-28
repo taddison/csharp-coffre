42588ce367f8839325c5b65160ea3793fc65e3c3
Tim Addison <git@tjaddison.com>
2019-01-28 18:32:02 -0500

    send basic card

--- teams-webhook/Program.cs
+++ teams-webhook/Program.cs
@@ -1,20 +1,32 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.FileExtensions;
 using Microsoft.Extensions.Configuration.Json;
+using Newtonsoft.Json;
 
 namespace teams_webhook
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var config = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", true, true)
                 .AddJsonFile("appsettings.local.json", true, true)
                 .Build();
 
-            Console.WriteLine($"Hello {config["TargetUri"]}");
+            var card = new {
+                type = "message",
+                text = "a little plain"
+            };
+            
+            var cardContent = new StringContent(JsonConvert.SerializeObject(card));
+
+            var client = new HttpClient();
+            var result = await client.PostAsync(config["TargetUri"], cardContent);
+            Console.WriteLine(result);
         }
     }
 }
